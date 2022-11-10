30fdb7fcfb9e9844497a10b7f7ac982fb8fbb3c5
qasm1 <qasm1@DESKTOP-UHPH6TR>
2022-11-10 15:39:46 +0200

    Add project files.

---
+++ api/Controllers/UsersController.cs
@@ -0,0 +1,31 @@
+﻿using api.DAL.context;
+using api.DAL.Entity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController :ControllerBase
+    {
+        public UsersController(DataContext Context)
+        {
+            _context = Context;
+        }
+        public DataContext _context { get; }
+        [HttpGet]
+        public async Task<IActionResult> GetUser()
+        {
+            var users = await _context.users.ToArrayAsync();
+            return Ok(users);
+        }
+        [HttpGet("users/{id}")]
+        public async Task<IActionResult> GetUserByID(int id)
+        {
+            var user =await _context.users.FindAsync(id);
+            return Ok(user);
+        }
+    }
+}
---
+++ api/DAL/context/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using api.DAL.Entity;
+using Microsoft.EntityFrameworkCore;
+
+namespace api.DAL.context
+{
+    public class DataContext:DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> opt):base(opt)
+        {
+
+        }
+       public DbSet<AppUser> users { set; get; }
+    }
+}
